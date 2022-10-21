9488d95029977229d8875a530b38e5b2f1dbc05d
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-10-21 17:20:28 +0700

    Add for loop input

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -6,26 +6,42 @@ using System.Threading.Tasks;
 
 namespace QuickSort
 {
-    //array of integers to hold values 
-    private int[] arr = new int[20];
-    private int cmp_count = 0;//  number of data comparasion
-    private int mov_count = 0; //number of data movements
-
-    //Number of element in array 
-    private int n; 
-
-
-
-
-    void input()
+    class Program
     {
-        while (true)
+        //array of integers to hold values 
+        private int[] arr = new int[20];
+        private int cmp_count = 0;//  number of data comparasion
+        private int mov_count = 0; //number of data movements
+
+        //Number of element in array 
+        private int n;
+
+
+
+
+        void input()
         {
-            
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array:");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can hame maximum 20 elements\n");
+            }
+            Console.WriteLine("\n======================");
+            Console.WriteLine("enter Array Elements");
+            Console.WriteLine("\n======================");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                
+            }
         }
-    }
-    internal class Program
-    {
+
         static void Main(string[] args)
         {
         }
