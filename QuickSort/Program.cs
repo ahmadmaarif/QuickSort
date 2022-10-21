6b19790ec269bd2d012c2257bced4e400b48ae8e
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-10-21 17:01:44 +0700

    add input while

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -7,8 +7,23 @@ using System.Threading.Tasks;
 namespace QuickSort
 {
     //array of integers to hold values 
+    private int[] arr = new int[20];
+    private int cmp_count = 0;//  number of data comparasion
+    private int mov_count = 0; //number of data movements
+
     //Number of element in array 
     private int n; 
+
+
+
+
+    void input()
+    {
+        while (true)
+        {
+            
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
