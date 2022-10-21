0214d4661c9a95eb87a7bd3aade0d1b2906ab5db
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-10-21 18:27:57 +0700

    Done

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -36,7 +36,7 @@ namespace QuickSort
             Console.WriteLine("\n======================");
 
             //get array elements
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
                 string s = Console.ReadLine();
@@ -52,10 +52,10 @@ namespace QuickSort
             arr[y] = temp;
 
         }
-        public void q_sort (int lowm, int high)
+        public void q_sort(int low, int high)
         {
-            int voit, i, j;
-            if (lowm > high)
+            int pivot, i, j;
+            if (low > high)
                 return;
 
 
@@ -66,10 +66,83 @@ namespace QuickSort
             i = low + 1;
             j = high;
 
-            voit = arr[i];
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //Search for element greater than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+                //Search for element smaller than pivot
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+                if (i < j)// if the greater elements is on the left of the elements
+                {
+                    //Swap the elements at index i with the elements at index
+                    swap(i, j);
+                    mov_count++;
+                }
+
+            }
+            // j now contains the index of the last elements in the sorted list
+            if (low < j)
+            { // move the pivot to the its correct position in the list       
+                swap(low, j);
+                mov_count++;
+            }
+            // sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            // sort the list on right of pivot using quick sort
+            q_sort(j + 1, high);
         }
+
+        void display()
+        {
+            Console.WriteLine("\n________________________________");
+            Console.WriteLine(" Sorted Array Elements ");
+            Console.WriteLine("________________________________");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]); 
+            }
+
+            Console.WriteLine("\nNumber of comparasion :" + cmp_count);
+            Console.WriteLine("\nNumber of data movements:" + mov_count);
+
+
+        }
+        int getSize()
+        {
+            return (n);
+        }
+
+
         static void Main(string[] args)
         {
+            //Declaring the oblect of the class
+            Program mylist = new Program();
+            //accept array elements 
+            mylist.input();
+            //Calling the sorting function
+            //First call to Quick sort Algorithm
+            mylist.q_sort(0, mylist.getSize() - 1);
+            //Display sorted array
+            mylist.display();
+            //to exit from the consoole
+            Console.WriteLine("\n\nPress Enter to exit.");
+            Console.Read();
+
         }
     }
 }
+
