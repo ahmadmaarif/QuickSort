e5b1ca16d70aa0592f7b9e6975b7fc95082721b2
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-10-21 17:30:22 +0700

    add swap input

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -38,10 +38,36 @@ namespace QuickSort
             //get array elements
             for(int i = 0; i < n; i++)
             {
-                
+                Console.Write("<" + (i + 1) + ">");
+                string s = Console.ReadLine();
+                arr[i] = Int32.Parse(s);
             }
         }
+        // Swap the elements at index x with the elements at index y
+        void swap(int x, int y)
+        {
+            int temp;
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
 
+        }
+        public void q_sort (int lowm, int high)
+        {
+            int voit, i, j;
+            if (lowm > high)
+                return;
+
+
+            //Partition list into two parts:
+            //one containing elements less than or equal to pivot
+            //Outher countaiming the elements greater then pivot
+
+            i = low + 1;
+            j = high;
+
+            voit = arr[i];
+        }
         static void Main(string[] args)
         {
         }
